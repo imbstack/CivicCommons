f2eaacf156282c484972d8cd072bf4923a5ca59e
Joe Fiorini <joe@faithfulgeek.org>
2010-06-26 21:31:03 -0400

    Generate mockups views directory

--- vendor/plugins/showoff/lib/generators/showoff/install/install_generator.rb
+++ vendor/plugins/showoff/lib/generators/showoff/install/install_generator.rb
@@ -2,8 +2,11 @@ module Showoff
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
+
       def install_showoff
+        empty_directory 'app/views/mockups/basic'
       end
+
 
     end
   end
