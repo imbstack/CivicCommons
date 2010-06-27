c26658688bea9cca42c46cf72dabab5f827ee758
Joe Fiorini <joe@faithfulgeek.org>
2010-06-26 21:50:31 -0400

    Add/copy showoff install generator templates

--- vendor/plugins/showoff/lib/generators/showoff/install/install_generator.rb
+++ vendor/plugins/showoff/lib/generators/showoff/install/install_generator.rb
@@ -1,12 +1,17 @@
 module Showoff
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path(File.dirname(__FILE__) + '/templates')
 
 
       def install_showoff
         empty_directory 'app/views/mockups/basic'
-      end
 
+        copy_file 'sample_mockup.html.erb', 'app/views/mockups/sample_mockup.html.erb'
+        copy_file 'alternate_layout.html.erb', 'app/views/mockups/basic/alternate_layout.html.erb'
+        copy_file 'showoff.css', 'public/stylesheets/showoff.css'
+
+      end
 
     end
   end
