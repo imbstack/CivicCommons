982f49eebfc1d05c83e1cf55d89311572ca281f5
Joe Fiorini <joe@faithfulgeek.org>
2010-06-26 20:18:11 -0400

    Make app boot with show off on Rails3

--- vendor/plugins/showoff/init.rb
+++ vendor/plugins/showoff/init.rb
@@ -1,5 +1,11 @@
 
 # Since MockupsController subclasses (and thus loads) ApplicationController, we need to make sure that
 # it gets reloaded in development. Without this, things will break after the first reload.
-ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
+if ActiveSupport::Dependencies.respond_to?(:autoload_once_paths)
+  load_once_paths_method = :autoload_once_paths
+else
+  load_once_paths_method = :load_once_paths
+end
+
+ActiveSupport::Dependencies.send(load_once_paths_method).delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
 
