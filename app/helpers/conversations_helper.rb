54a7869e51cde1e2a9d8439ba88f1861808c2f89
Mark W. Schumann <mark@criticalresults.com>
2010-07-26 12:06:27 -0400

    Add-issue dropdown now includes only issues *not* already selected.

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,9 @@
 module ConversationsHelper
+
+def confirm_add_issue_link(id)
+#  link_to 'Actually add issue I', :url => { :controller => "foo", :action => "bar", :id => id }, :remote => true
+#  link_to 'Actually add issue II', :url => { :controller => "foo", :action => "bar", :id => id }, :success => "alert('Win!');$('xyzzy').show()", :failure => "alert('Lose!');$('xyzzy').show()", :class => 'privileged', :remote => true
+#   = <a href="javascript:void();" onClick="alert('hi!');">Actually add issue III</a>
+end
+
 end
