256710c302eddc4d6421a49f4444b7005699b031
Joe Fiorini <joe@faithfulgeek.org>
2010-07-26 18:24:01 -0400

    Delete bad code

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -3,16 +3,5 @@ $(document).ready(function(){
     $('[placeholder]').placeholder({className: 'placeholder'});
   }
 })
-alert("Loading chooseissue!")
-function chooseissue(n)
-{
-  alert("hi!")
-#  @chosenissues.push(n)
-#  alert("hi!")
-#  page.replace_html($("div#chosenissues"), :partial => "chosenissues")
-#  alert("hi!")
-#  $("#addissue").hide()
-#  alert("hi!")
-#  $("#addissueprompt").show()
-#  alert("hi!")
-}
+
+
