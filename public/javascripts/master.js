b366f02aa532617014b1d6c0021583409fd56e21
Joe Fiorini <joe@faithfulgeek.org>
2010-07-29 21:15:54 -0400

    Markup conversation input tabs

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -2,6 +2,20 @@ $(document).ready(function(){
   if(!Modernizr.input.placeholder) {
     $('[placeholder]').placeholder({className: 'placeholder'});
   }
+
+  commentsTab.run();
 })
+
+var commentsTab = $.sammy(function(){
+
+    this.get("#/question/new", function(){
+      $("#comment_text").hide();
+      $("#file_url").hide();
+      $("#link_url").hide();
+      $("#video_url").hide();
+      $("#question_text").show();
+
+    });
+});
 
 
